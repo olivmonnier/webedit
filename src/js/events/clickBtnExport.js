1466d45dd22520162f207c5e1049624be0285067
Olivier Monnier <omonnier.dev@gmail.com>
2017-02-13 14:28:54 +0100

    in progress

--- src/js/events/clickBtnExport.js
+++ src/js/events/clickBtnExport.js
@@ -1,13 +1,15 @@
 const basicModal = require('basicmodal');
 import getContents from '../utils/getContents';
 import getClosest from '../utils/getClosest';
+import CodeMirror from 'codemirror';
+require('codemirror/mode/htmlmixed/htmlmixed');
 
 export default function(elem, container) {
   elem.addEventListener('click', function(e) {
     const content = getContents(container);
 
     basicModal.show({
-      body: '<pre>' + content + '</pre>',
+      body: '<textarea>' + content + '</textarea>',
       class: 'modal-export',
       buttons: {
         action: {
@@ -15,6 +17,16 @@ export default function(elem, container) {
           fn: basicModal.close
         }
       }
-    })
+    });
+
+    if (basicModal.visible()) {
+      const textarea = document.getElementsByTagName('textarea')[0];
+      const editor = CodeMirror.fromTextArea(textarea, {
+        lineNumbers: true,
+        lineWrapping: true,
+        mode: 'htmlmixed',
+        tabSize: '2'
+      });
+    }
   });
 }
